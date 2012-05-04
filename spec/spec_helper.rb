ec2e47750bb84eee1528fbde1220803b9527bfd9
Christian Bäuerlein <fabrik42@gmail.com>
2012-05-04 11:01:08 +0200

    adds support for hidden navteq host file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,12 @@ require 'bundler'
 Bundler.require(:default, :test)
 
 RSpec.configure do |config|
+end
 
+if File.exist?('.navteq_host')
+  class Routing::Adapter::Navteq
+    def host
+      File.read('.navteq_host')
+    end
+  end
 end
