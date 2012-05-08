5d6dd427f55d7162ee8a17ca784f078fab955579
Christian Bäuerlein <fabrik42@gmail.com>
2012-05-08 11:20:05 +0200

    allow test adapter to accept any geo point compatible items

--- lib/routing/adapter/test.rb
+++ lib/routing/adapter/test.rb
@@ -1,12 +1,12 @@
 class Routing
   module Adapter
 
-    # Simple test adapter, that just returns the same {GeoPoint}
-    # that it recieved.
+    # Simple test adapter, that just returns a copy of the {GeoPoint}s array
+    # that it recieved (just copies the lat, lng values).
     class Test
 
       def calculate(geo_points)
-        geo_points
+        geo_points.collect{|point| GeoPoint.new(:lat => point.lat, :lng => point.lng) }
       end
     end
 
--- spec/routing/adapter/test_spec.rb
+++ spec/routing/adapter/test_spec.rb
@@ -11,13 +11,16 @@ describe Routing::Adapter::Test do
   end
 
   describe '#calculate' do
-    
+
     let(:geo_points){ [Struct.new(:lat, :lng).new(1, 2), Struct.new(:lat, :lng).new(3, 4), Struct.new(:lat, :lng).new(5, 6)] }
-    
-    it 'returns the same geo points that are passed to the method' do
-      subject.calculate(geo_points).should == geo_points
+
+    it 'returns the coordinates that are passed to the method' do
+      subject.calculate(geo_points).each_with_index do |new_geo_point, index|
+        new_geo_point.lat.should eql(geo_points[index].lat)
+        new_geo_point.lng.should eql(geo_points[index].lng)
+      end
     end
-    
+
   end
 
 end
